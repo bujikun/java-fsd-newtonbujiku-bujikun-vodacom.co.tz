3e91cb83e3f1771ece9079428d4257a7f365d83b
Newton Bujiku <bujikun@vodacom.co.tz>
2023-03-25 12:56:23 +0300

    Initial commit for phase 3 project Add product entity Add webjars dependencies

---
+++ Phase 3/code/src/main/java/tz/co/vodacom/bujikun/sportyshoes/entity/Product.java
@@ -0,0 +1,11 @@
+package tz.co.vodacom.bujikun.sportyshoes.entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Product {
+}
---
+++ Phase 3/code/src/test/java/tz/co/vodacom/bujikun/sportyshoes/SportyshoesApplicationTests.java
@@ -0,0 +1,13 @@
+package tz.co.vodacom.bujikun.sportyshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyshoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
