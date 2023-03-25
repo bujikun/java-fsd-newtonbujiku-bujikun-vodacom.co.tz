ec7f8d4a1636665796680224f67b09b015fdbf79
Newton Bujiku <bujikun@vodacom.co.tz>
2023-03-25 13:33:38 +0300

    Add exception for non existing product Implement ProductService

--- Phase 3/code/src/main/java/tz/co/vodacom/bujikun/sportyshoes/service/ProductService.java
+++ Phase 3/code/src/main/java/tz/co/vodacom/bujikun/sportyshoes/service/ProductService.java
@@ -4,20 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tz.co.vodacom.bujikun.sportyshoes.entity.Product;
+import tz.co.vodacom.bujikun.sportyshoes.exception.ProductNotFoundException;
+import tz.co.vodacom.bujikun.sportyshoes.repository.ProductRepository;
 
 import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ProductService implements GenericService<Product,Integer> {
+    private final ProductRepository productRepository;
     @Override
     public void createNew(Product product) {
-
+        productRepository.save(product);
     }
 
     @Override
     public void delete(Integer id) {
-
+        productRepository.delete(findById(id));
     }
 
     @Override
@@ -27,11 +30,12 @@ public class ProductService implements GenericService<Product,Integer> {
 
     @Override
     public Product findById(Integer id) {
-        return null;
+        return productRepository.findById(id)
+                .orElseThrow(()-> new ProductNotFoundException("Product Not Found"));
     }
 
     @Override
     public List<Product> findAll() {
-        return null;
+        return productRepository.findAll();
     }
 }
