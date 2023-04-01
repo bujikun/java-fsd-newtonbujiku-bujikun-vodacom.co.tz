e04a56ad6359f7bc94038deed215431c23d82ad2
Newton Bujiku <bujikun@vodacom.co.tz>
2023-04-01 20:17:46 +0300

    Add logic to link product and category from product side

--- Phase 3/code/src/main/java/tz/co/vodacom/bujikun/sportyshoes/controller/ProductController.java
+++ Phase 3/code/src/main/java/tz/co/vodacom/bujikun/sportyshoes/controller/ProductController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import tz.co.vodacom.bujikun.sportyshoes.dao.ProductCategoryDAO;
 import tz.co.vodacom.bujikun.sportyshoes.service.CategoryService;
 import tz.co.vodacom.bujikun.sportyshoes.service.ProductService;
 
@@ -14,7 +15,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
-    //private final CategoryService categoryService;
+    private final CategoryService categoryService;
+    private final ProductCategoryDAO productCategoryDAO;
 
     @GetMapping
     public String getAll(Model model){
@@ -41,5 +43,31 @@ public class ProductController {
         var product = productService.findById(id);
         model.addAttribute("product",product);
         return "product/view";
+    }
+
+    @GetMapping(value = {"/{id}/link-category","/{id}/link-category/"})
+    public String getLinkProductCategories(@PathVariable("id")Integer productId, Model model){
+        var product = productService.findById(productId);
+        var productCategories = product.getCategories();
+        var allCategories = categoryService.findAll();
+        model.addAttribute("product",product);
+        model.addAttribute("productCategories",productCategories);
+        model.addAttribute("allCategories",allCategories);
+        return "product/link-category";
+    }
+
+    @PostMapping(value = {"/link-category","/link-category/"})
+    public String postLinkCategoryProducts(@RequestParam(name = "categoryId") Integer categoryId,
+                                           @RequestParam(name = "productId") Integer productId,
+                                           Model model){
+        //TODO could generate exception, handle for error
+        productCategoryDAO.linkProductCategory(productId,categoryId);
+        var product = productService.findById(productId);
+        var productCategories = product.getCategories();
+        var allCategories = categoryService.findAll();
+        model.addAttribute("product",product);
+        model.addAttribute("productCategories",productCategories);
+        model.addAttribute("allCategories",allCategories);
+        return "product/link-category";
     }
 }
