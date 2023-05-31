3b4ae4eda3363f7ef6005f327703842197f7db7a
Newton Bujiku <bujikun@vodacom.co.tz>
2023-05-31 19:51:11 +0300

    Initial configuration for phase 4 kitchen stories project

---
+++ Phase 4/code/src/main/java/tz/co/vodacom/bujikun/kitchenstories/KitchenStoriesApplication.java
@@ -0,0 +1,13 @@
+package tz.co.vodacom.bujikun.kitchenstories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenStoriesApplication.class, args);
+	}
+
+}
---
+++ Phase 4/code/src/test/java/tz/co/vodacom/bujikun/kitchenstories/KitchenStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package tz.co.vodacom.bujikun.kitchenstories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenStoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
