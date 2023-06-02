cf7f7d482eb1d19cb0df663a4da085d78a0e70bf
Newton Bujiku <bujikun@vodacom.co.tz>
2023-06-02 11:52:47 +0300

    Change cart items display from list to table Add logic to remove items from cart

--- Phase 4/code/front-end/src/components/cart/Cart.jsx
+++ Phase 4/code/front-end/src/components/cart/Cart.jsx
@@ -1,37 +1,57 @@
-import { useSelector } from "react-redux";
-import { selectAllCartItems } from "../../redux/features/cart/cartSlice";
-
+import { useDispatch, useSelector } from "react-redux";
+import { removeCartItem, selectAllCartItems } from "../../redux/features/cart/cartSlice";
 const Cart = () => {
   const cartItems = useSelector(selectAllCartItems);
-  console.log(cartItems);
+  const dispatch = useDispatch();
   return (
-    <div
-      className="my-4 d-flex flex-column justify-content- align-items-center"
-      style={{ width: "100%" }}
-    >
-      <span className="display-6">Items</span>
-      <div className="list-group my-3">
-        {cartItems.map((item, index) => (
-          <div
-            key={item.id}
-            className={
-              "list-group-item " +
-              (index % 2 === 0 ? "" : "list-group-item-success")
-            }
-          >
-            <span className="d-inline-block me-3 px-3">{index + 1}.</span>
-            <span className="d-inline-block me-3 px-3">{item.name}</span>
-            <span className="d-inline-block me-3 px-3">${item.price}</span>
-            <span className="d-inline-block me-3 px-3">({item.quantity})</span>
-            <div className="d-inline-block me-3 px-2 bg-danger"
-              style={{
-                "color": "white",
-                "borderRadius":"20px"
-              }}>X</div>
+    <main className="container">
+      <section className="row">
+        <div
+          className="my-4 d-flex flex-column justify-content- align-items-center"
+          style={{ width: "100%" }}
+        >
+          <span className="display-6">Cart Items</span>
+        </div>
+        <div className="col-sm-12 col-md-10 col-lg-10 col-xl-10 offset-md-1 offset-lg-1 offset-xl-1">
+          <div>
+            <table className="table table-striped table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th>Index</th>
+                  <th>Food Name</th>
+                  <th>Quantity</th>
+                  <th>Remove</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item, index) => (
+                  <tr key={item.id}>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>{item.quantity}</td>
+                    <td>
+                      <div
+                        className="d-inline-block me-3 px-2 bg-danger"
+                        style={{
+                          color: "white",
+                          borderRadius: "20px",
+                          cursor: "pointer",
+                          fontWeight: "600",
+                        }}
+                        onClick={()=>dispatch(removeCartItem(item.id))}
+                      >
+                        X
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot></tfoot>
+            </table>
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      </section>
+    </main>
   );
 };
 export default Cart;
--- Phase 4/code/front-end/src/redux/features/cart/cartSlice.js
+++ Phase 4/code/front-end/src/redux/features/cart/cartSlice.js
@@ -6,35 +6,29 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-      addToCart(state, action) {
-          const { id } = action.payload;
-          const food = state.cartItems.find(food => food.id === id);
-          if (food) {
-              food.quantity += 1;
-              const index = state.cartItems.indexOf(food);
-              state.cartItems[index] = food;
-              return;
-          } 
-          state.cartItems.push({...action.payload,quantity:0});
-    //       let isExistingItem = false;
-    //   const { id } = action.payload.id;
-    //   const food = state.cartItems.forEach((food, index) => {
-    //     if (food.id === id) {
-    //         food.quantity += 1;
-    //         isExistingItem = true;
-    //       console.log(index);
-    //       return;
-    //     }
-    //   });
-    //       if (isExistingItem) {
-    //           return;
-    //       }
-    //       const 
-    //   state.cartItems.push(action.payload);
+    addToCart(state, action) {
+      const { id } = action.payload;
+      const food = state.cartItems.find((food) => food.id === id);
+      if (food) {
+        food.quantity += 1;
+        const index = state.cartItems.indexOf(food);
+        state.cartItems[index] = food;
+        return;
+      }
+      state.cartItems.push({ ...action.payload, quantity: 1 });
+    },
+    removeCartItem(state, action) {
+      const filteredItems = state.cartItems.filter(
+        (item) => item.id !== action.payload
+      );
+        console.log(action.payload);
+        state.cartItems = filteredItems;
+
+
     },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeCartItem } = cartSlice.actions;
 export const selectAllCartItems = (state) => state.cart.cartItems;
 export const cartReducer = cartSlice.reducer;
