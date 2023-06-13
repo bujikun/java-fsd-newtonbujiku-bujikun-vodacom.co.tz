483a3464ed93d0e7c3018725ae0ff80938fae30c
Newton Bujiku <bujikun@vodacom.co.tz>
2023-06-13 17:43:30 +0300

    Add registration form and the required validation

--- Phase 4b/code/event-finder/src/components/user/RegistrationForm.jsx
+++ Phase 4b/code/event-finder/src/components/user/RegistrationForm.jsx
@@ -1,16 +1,99 @@
-import {
-  Container,
-  Box,
-  Typography,
-  TextField,
-  Button,
-} from "@mui/material";
+import { Container, Box, Typography, TextField, Button } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { useState } from "react";
+const initialUser = {
+  firstName: "",
+  lastName: "",
+  username: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const initialError = {
+  firstName: {
+    isError: false,
+    errorMessage: "",
+  },
+  lastName: {
+    isError: false,
+    errorMessage: "",
+  },
+  username: {
+    isError: false,
+    errorMessage: "",
+  },
+  password: {
+    isError: false,
+    errorMessage: "",
+  },
+  confirmPassword: {
+    isError: false,
+    errorMessage: "",
+  },
+};
 
 const RegistrationForm = () => {
-    const handleSubmit = (e) => {
-      e.preventDefault();
-    };
+  const [user, setUser] = useState(initialUser);
+  const [error, setError] = useState(initialError);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const validateInput = (inputName, value, saneName, length) => {
+    if (value.trim().length <= Number(length)) {
+      setError({
+        ...error,
+        [inputName]: {
+          ...error.inputName,
+          isError: true,
+          errorMessage: `${saneName} length must be greater than ${length}`,
+        },
+      });
+    } else {
+      setError({
+        ...error,
+        [inputName]: {
+          ...error.inputName,
+          isError: false,
+          errorMessage: "",
+        },
+      });
+    }
+  };
+
+  const validatePassword = (inputName, value) => {
+    if (inputName === "confirmPassword") {
+      if (user.password !== value) {
+        setError({
+          ...error,
+          [inputName]: {
+            ...error.inputName,
+            isError: true,
+            errorMessage: "Passwords do not match",
+          },
+        });
+        console.log(user.password, user.confirmPassword);
+      } else {
+        setError({
+          ...error,
+          [inputName]: {
+            ...error.inputName,
+            isError: false,
+            errorMessage: "",
+          },
+        });
+      }
+    }
+  };
+
+  const handleChange = (e, saneName, length) => {
+    const value = String(e.target.value).trim();
+    const inputName = e.target.name;
+    validateInput(inputName, value, saneName, length);
+    validatePassword(inputName, value);
+    setUser({ ...user, [inputName]: value });
+  };
+
   return (
     <Container maxWidth="xs" component="main">
       <Box
@@ -19,7 +102,6 @@ const RegistrationForm = () => {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          //   textAlign:"center"
         }}
       >
         <Typography component="h1" variant="h5">
@@ -36,6 +118,10 @@ const RegistrationForm = () => {
                 id="firstName"
                 label="First Name"
                 autoFocus
+                value={user.firstName}
+                onChange={(e) => handleChange(e, "First Name", 5)}
+                error={error.firstName.isError}
+                helperText={error.firstName.errorMessage}
               />
             </Grid>
             <Grid xs={12} sm={6}>
@@ -45,7 +131,11 @@ const RegistrationForm = () => {
                 required
                 fullWidth
                 id="lastName"
+                value={user.lastName}
+                onChange={(e) => handleChange(e, "Last Name", 5)}
                 label="Last Name"
+                error={error.lastName.isError}
+                helperText={error.lastName.errorMessage}
               />
             </Grid>
             <Grid xs={12}>
@@ -56,6 +146,10 @@ const RegistrationForm = () => {
                 id="username"
                 autoComplete="username"
                 label="Username"
+                value={user.username}
+                onChange={(e) => handleChange(e, "Username", 5)}
+                error={error.username.isError}
+                helperText={error.username.errorMessage}
               />
             </Grid>
             <Grid xs={12}>
@@ -66,7 +160,11 @@ const RegistrationForm = () => {
                 label="Password"
                 type="password"
                 id="password"
+                value={user.password}
+                onChange={(e) => handleChange(e, "Password", 8)}
                 autoComplete="new-password"
+                error={error.password.isError}
+                helperText={error.password.errorMessage}
               />
             </Grid>
             <Grid xs={12}>
@@ -78,6 +176,10 @@ const RegistrationForm = () => {
                 type="password"
                 id="confirmPassword"
                 autoComplete="new-password"
+                value={user.confirmPassword}
+                onChange={(e) => handleChange(e, "Password", 8)}
+                error={error.confirmPassword.isError}
+                helperText={error.confirmPassword.errorMessage}
               />
             </Grid>
           </Grid>
